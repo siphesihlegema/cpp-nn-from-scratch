4ea74f8e1b50fe66b9a8c4901e13899ce74236d1
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-09-22 22:43:14 +0200

    feater(Matrix): add transpose() implamentation
    used a double for loop to map matrix to the transpose. this my be slow,
    but i dont know i i can make it faster for now.

--- source/Matrix.cpp
+++ source/Matrix.cpp
@@ -4,10 +4,11 @@
 #include "../include/Matrix.hpp"
 
 Matrix::Matrix(std::size_t rows, std::size_t col, float init_value)
-    : rows_(rows), col_(col), data_(std::vector(rows * col, init_value)) {}
+    : rows_(rows), col_(col),
+      data_(std::move(std::vector(rows * col, init_value))) {}
 
 Matrix::Matrix(std::size_t rows, std::size_t col, std::vector<float> data)
-    : rows_(rows), col_(col), data_(data) {}
+    : rows_(rows), col_(col), data_(std::move(data)) {}
 
 float &Matrix::operator()(std::size_t r, std::size_t c) {
   return data_[r * col_ + c];
@@ -15,4 +16,16 @@ float &Matrix::operator()(std::size_t r, std::size_t c) {
 
 const float &Matrix::operator()(std::size_t r, std::size_t c) const {
   return data_[r * col_ + c];
+}
+
+Matrix Matrix::transpose() const {
+  std::vector<float> transposed(rows_ * col_);
+
+  for (std::size_t r = 0; r < rows_; ++r) {
+    for (std::size_t c = 0; c < col_; ++c) {
+      transposed[c * rows_ + r] = data_[r * col_ + c];
+    }
+  }
+
+  return Matrix(col_, rows_, std::move(transposed));
 }
