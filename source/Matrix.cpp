a7c807b73d29373a80493df869c724c64ebedc6f
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-09-22 21:15:17 +0200

    feater(Matrix): add operator() implementation
    returened the float from the vector using row-major mapping to map
    matrix into a vector

--- source/Matrix.cpp
+++ source/Matrix.cpp
@@ -8,3 +8,11 @@ Matrix::Matrix(std::size_t rows, std::size_t col, float init_value)
 
 Matrix::Matrix(std::size_t rows, std::size_t col, std::vector<float> data)
     : rows_(rows), col_(col), data_(data) {}
+
+float &Matrix::operator()(std::size_t r, std::size_t c) {
+  return data_[r * col_ + c];
+}
+
+const float &Matrix::operator()(std::size_t r, std::size_t c) const {
+  return data_[r * col_ + c];
+}
