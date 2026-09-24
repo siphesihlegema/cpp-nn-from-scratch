c6917da9ec2c212caea0c9ade883781446aa8930
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-09-24 19:21:55 +0200

    feater(Matrix): declare Hadamerd product
    made the function static so to distinct it from the other operation
    functions, in a way to indicate that this function won't modify any
    matrix (Matrix c = Hadamerd(a, b))
    
    another reason for it to be static is to allow it to be called from the
    class not under any instance.

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -11,6 +11,7 @@ private:
   std::vector<float> data_;
 
 public:
+  Matrix();
   Matrix(std::size_t rows, std::size_t col, float init_value = 0.0f);
   Matrix(std::size_t rows, std::size_t col, std::vector<float> data);
 
@@ -29,12 +30,17 @@ public:
   // in place operations
   Matrix &operator+=(const Matrix &rhs);
   Matrix &operator-=(const Matrix &rhs);
+
+  // Hadamard product
+  static Matrix hadamard(const Matrix &a, const Matrix &b);
 };
+
 // binary operations
 inline Matrix operator+(Matrix lhs, const Matrix &rhs) {
   lhs += rhs;
   return lhs;
 }
+
 inline Matrix operator-(Matrix lhs, const Matrix &rhs) {
   lhs -= rhs;
   return lhs;
